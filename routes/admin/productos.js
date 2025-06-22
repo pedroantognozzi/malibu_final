203772a6335f64a20a18e52c5862ac3f32f1eb3c
Pedro Antognozzi <pedroantognozzi@gmail.com>
2025-06-22 02:25:23 -0300

    armado tabla de productos

--- routes/admin/productos.js
+++ routes/admin/productos.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var productosModel = require('../../models/productosModel');
 
-var usuariosModel = require('./../../models/usuariosModel');
 
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+
+    var productos = await productosModel.getProductos();
 
     res.render('admin/productos', {
         layout: 'admin/layout',
-        usuario: req.session.nombre
+        usuario: req.session.nombre,
+        productos
     });
 });
+
+router.get('/eliminar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    await productosModel.deleteProductoById(id);
+    res.redirect('/admin/productos');
+});
+
 
 module.exports = router;
