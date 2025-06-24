95ffd23c85adaf34794d5dfb2c45fea5f9c84a75
Pedro Antognozzi <pedroantognozzi@gmail.com>
2025-06-24 14:25:45 -0300

    base actualizada despues de la prueba

--- base_malibu(1).sql
+++ base_malibu(1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 23, 2025 at 07:31 PM
+-- Generation Time: Jun 24, 2025 at 05:23 PM
 -- Server version: 9.1.0
 -- PHP Version: 8.3.14
 
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `productos` (
   `cuerpo` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   `img_id` varchar(250) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `productos`
@@ -46,7 +46,7 @@ INSERT INTO `productos` (`id`, `titulo`, `subtitulo`, `cuerpo`, `img_id`) VALUES
 (3, 'Malibu Splash', 'Malibu Splash is a sparkling malt beverage that combines fruity flavors with a hint of coconut. Known for its freshness and tropical flavor, it\'s available in several flavors, including strawberry, pineapple, lime, passion fruit, and watermelon, all blended with coconut.', '', 'zdtz4ojcv7sif3mjby31'),
 (11, 'Ready to Serve', 'Malibu Ready-to-Serve (RTS) cocktails are pre-mixed, bottled cocktails made with Malibu white rum, offering convenience and portability for enjoying popular drinks like Strawberry Daiquiri, Pineapple Bay Breeze, and Rum Punch. They are designed for easy serving over ice or straight from the bottle, making them ideal for gatherings or on-the-go occasions. ', NULL, 'sc7taxzrsayi37zfusrc'),
 (12, 'Malibu Beer', 'Malibu Beer is a golden lager infused with a hint of natural coconut flavor, produced in the Caribbean and imported to the US by United States Beverage. It has a 5% ABV and is designed to offer a light, tropical taste. ', NULL, 'znc4e3cprysnceht6g3g'),
-(13, 'Classic Malibu', 'Malibu is a coconut-flavored liqueur made with white rum, known for its smooth and sweet taste. It\'s a popular base for tropical cocktails like piña coladas and is often enjoyed on its own with ice. The product was created in 1978 and is known for its distinctive blend of Caribbean rum and natural coconut flavor. ', NULL, 'cpa14n2rrvcymwabvwwm');
+(16, 'Classic Malibu', 'Malibu is a coconut-flavored liqueur made with white rum, known for its smooth and sweet taste. It\'s a popular base for tropical cocktails like piña coladas and is often enjoyed on its own with ice. The product was created in 1978 and is known for its distinctive blend of Caribbean rum and natural coconut flavor.', NULL, 'z3qg19relh0xyz8ysjc0');
 
 -- --------------------------------------------------------
 
